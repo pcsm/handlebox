1c5bbf854d53fdeaaff4250540aeb8c83f0656b7
Jason Grlicky <jason@paracosm.us>
2017-05-06 14:54:41 -0700

    Moved over to using a BTreeMap internally

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,34 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 pub type Handle = usize;
 
-/// A HashMap that keeps track of unused keys
+/// A map-like collection that keeps track of unused keys
 #[derive(Default)]
 pub struct HandleBox<V> {
-    pub hash_map: HashMap<Handle, V>,
+    pub map: BTreeMap<Handle, V>,
     discarded_handles: Vec<Handle>, // Handles that have been added but later removed
 }
 
 impl<V> HandleBox<V> {
     pub fn new() -> HandleBox<V> {
         HandleBox {
-            hash_map: HashMap::new(),
+            map: BTreeMap::new(),
             discarded_handles: vec![],
         }
     }
 
     fn new_handle(&mut self) -> Handle {
-        self.discarded_handles.pop().unwrap_or(self.hash_map.values().len())
+        self.discarded_handles.pop().unwrap_or(self.map.values().len())
     }
 
     pub fn add(&mut self, value: V) -> Handle {
         let h = self.new_handle();
-        self.hash_map.insert(h, value);
+        self.map.insert(h, value);
         h
     }
 
     pub fn remove(&mut self, handle: &Handle) -> Option<V> {
-        let result = self.hash_map.remove(handle);
+        let result = self.map.remove(handle);
         if result.is_some() {
             self.discarded_handles.push(*handle)
         }
@@ -36,15 +36,15 @@ impl<V> HandleBox<V> {
     }
 
     pub fn get(&self, handle: &Handle) -> Option<&V> {
-        self.hash_map.get(handle)
+        self.map.get(handle)
     }
 
     pub fn get_mut(&mut self, handle: &Handle) -> Option<&mut V> {
-        self.hash_map.get_mut(handle)
+        self.map.get_mut(handle)
     }
 
-    pub fn hash_map(&self) -> &HashMap<Handle, V> {
-        &self.hash_map
+    pub fn internal_map(&self) -> &BTreeMap<Handle, V> {
+        &self.map
     }
 }
 
@@ -55,26 +55,26 @@ mod tests {
     #[test]
     fn test_new() {
         let c = HandleBox::<i32>::new();
-        assert!(c.hash_map().is_empty());
+        assert!(c.internal_map().is_empty());
     }
 
     #[test]
     fn test_add_remove() {
         let mut c = HandleBox::new();
         let h1 = c.add(888);
-        assert!(!c.hash_map().is_empty());
+        assert!(!c.internal_map().is_empty());
         assert_eq!(c.get(&h1).unwrap(), &888);
 
         let h2 = c.add(999);
-        assert_eq!(c.hash_map().values().len(), 2);
+        assert_eq!(c.internal_map().values().len(), 2);
         assert_eq!(c.get(&h2).unwrap(), &999);
 
         c.remove(&h2);
-        assert_eq!(c.hash_map().values().len(), 1);
+        assert_eq!(c.internal_map().values().len(), 1);
         assert!(c.get(&h2).is_none());
 
         c.remove(&h1);
-        assert!(c.hash_map().is_empty());
+        assert!(c.internal_map().is_empty());
         assert!(c.get(&h1).is_none());
     }
 }
