496b25ac0f41f3edf1cf44c27739f24b67e2c141
Jason Grlicky <jason@paracosm.us>
2017-12-16 16:44:15 -0800

    Improved documentation

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,19 @@ use std::collections::BTreeMap;
 /// The keys used in a `HandleBox`. A typedef for `u32`.
 pub type Handle = u32;
 
-/// A map-like collection that reuses unused keys. Hard-coded to use `u32` keys right now.
-/// Exposes the internal `map` field if you need to access it directly.
+/// A map-like collection that reuses unused keys. Hard-coded to use `u32` `Handle`s right now.
 #[derive(Default)]
 pub struct HandleBox<V> {
+    /// If you need to directly access the internal `BTreeMap`, you can use this field.
+    ///
+    /// Do not remove items from the internal map directly, because then the `Handle`s
+    /// can't be reused. Use the `HandleBox.remove()` method instead.
     pub map: BTreeMap<Handle, V>,
     discarded_handles: Vec<Handle>, // Handles that have been added but later removed
 }
 
 impl<V> HandleBox<V> {
+    /// Create an empty `HandleBox`
     pub fn new() -> HandleBox<V> {
         HandleBox {
             map: BTreeMap::new(),
@@ -19,16 +23,17 @@ impl<V> HandleBox<V> {
         }
     }
 
-    fn new_handle(&mut self) -> Handle {
-        self.discarded_handles.pop().unwrap_or(self.map.values().len() as Handle)
-    }
-
+    /// Add a new value to the `HandleBox`. Note that an associated `Handle` for
+    /// this value is automatically created and returned. Duplicate insertions
+    /// are possible if you insert the same value twice.
     pub fn add(&mut self, value: V) -> Handle {
         let h = self.new_handle();
         self.map.insert(h, value);
         h
     }
 
+    /// Remove the value associated with this `Handle`. If the value was removed,
+    /// `Some<V>` is returned. If not, `None` is returned.
     pub fn remove(&mut self, handle: &Handle) -> Option<V> {
         let result = self.map.remove(handle);
         if result.is_some() {
@@ -37,12 +42,18 @@ impl<V> HandleBox<V> {
         result
     }
 
+    /// Get a shared reference to the value with this `Handle`.
     pub fn get(&self, handle: &Handle) -> Option<&V> {
         self.map.get(handle)
     }
 
+    /// Get a mutable reference to the value with this `Handle`.
     pub fn get_mut(&mut self, handle: &Handle) -> Option<&mut V> {
         self.map.get_mut(handle)
+    }
+    
+    fn new_handle(&mut self) -> Handle {
+        self.discarded_handles.pop().unwrap_or(self.map.values().len() as Handle)
     }
 }
 
